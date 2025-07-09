f7bdd91a012fb5ea7776ca5ab4e7c0b8f2d5b660
firstname lastname <manojkumar.a4004@gmail.com>
2025-07-09 17:03:49 +0530

    Added subnet_id for EC2 instance

--- main.tf
+++ main.tf
@@ -3,24 +3,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.medium"
-  key_name      = "jenkins"         # Replace with your actual key pair name
+  key_name      = "jenkins"
+
+  subnet_id     = "subnet-0c4562377d6219710"  
 
   tags = {
     Name = "JenkinsServer"
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo amazon-linux-extras enable corretto11
-              sudo yum install java-11-amazon-corretto -y
-              sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-              sudo yum install jenkins -y
-              sudo systemctl enable jenkins
-              sudo systemctl start jenkins
-              EOF
+  user_data = <<-EOT
+    #!/bin/bash
+    sudo yum update -y
+    sudo amazon-linux-extras enable corretto11
+    sudo yum install java-11-amazon-corretto -y
+    sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+    sudo yum install jenkins -y
+    sudo systemctl enable jenkins
+    sudo systemctl start jenkins
+  EOT
 }
 
